57f64c7f70fc691d3f4433c0c2bbcbcf222448bb
LorenzoLiwanag <lorenzonlliwanag@outlook.com>
2025-03-16 14:48:37 -0600

    testing display numbers on click and added clear button function

--- calculator.js
+++ calculator.js
@@ -1,2 +1,16 @@
 const display = document.querySelector("#calculator-display");
 
+const numberButtons = document.querySelectorAll(".number-btn");
+const clearButton = document.querySelector("#clear-btn");
+
+numberButtons.forEach(numberButton => {
+    display.value = "";
+    numberButton.addEventListener("click", () => {
+        display.value += numberButton.innerHTML;
+    })
+});
+
+
+clearButton.addEventListener("click", () =>{
+    display.value = "";
+})
