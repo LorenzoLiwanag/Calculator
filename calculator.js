25acaae2954833e44e97d1208075a609afa97e1c
LorenzoLiwanag <lorenzonlliwanag@outlook.com>
2025-03-18 17:56:52 -0600

    Using JS to display buttons rather than html?

--- calculator.js
+++ calculator.js
@@ -1,16 +1,59 @@
 const display = document.querySelector("#calculator-display");
-
 const numberButtons = document.querySelectorAll(".number-btn");
 const clearButton = document.querySelector("#clear-btn");
+const numbersContainer = document.querySelector("#numbers-container");
+const operationsContainer = document.querySelector("#operations-container");
 
-numberButtons.forEach(numberButton => {
-    display.value = "";
-    numberButton.addEventListener("click", () => {
-        display.value += numberButton.innerHTML;
+const displayNumbers = () => {
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    const numberSize = numbers.length;
+
+    for (let i = 0; i < numberSize; i++) {
+        const numberBtn = document.createElement("button")
+        numberBtn.classList.add("number-btn");
+        numberBtn.textContent = numbers[i];
+        numbersContainer.appendChild(numberBtn);
+    }
+}
+
+const displayOperations = () => {
+    const operations = ['+', '-', '*', '/',];
+    const operationsSize = operations.length;
+
+    for (let i = 0; i < operationsSize; i++) {
+        const operationBtn = document.createElement("button");
+        operationBtn.classList.add("operation-btn");
+        operationBtn.textContent = operations[i];
+        operationsContainer.appendChild(operationBtn);
+
+    }
+
+}
+
+
+const addNumberEventListeners = () => {
+    document.querySelectorAll("number-btn").forEach(numberButton => {
+        numberButton.addEventListener("click", () => {
+            display.value += numberButton.innerHTML;
+        })
+    });
+
+
+    clearButton.addEventListener("click", () => {
+        display.value = "";
     })
-});
+}
 
+const addOperationEventListeners = () => {
+    document.querySelectorAll(".operation-btn").forEach(operationButton => {
+        operationButton.addEventListener("click", () => {
+            display.value += operationButton.innerHTML;
+        });
+    });
 
-clearButton.addEventListener("click", () => {
-    display.value = "";
-})
+}
+
+displayNumbers();
+displayOperations();
+addNumberEventListeners();
+addOperationEventListeners();
