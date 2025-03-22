cf936cd150fa6e8c79f41612542101747e31bbaf
LorenzoLiwanag <124101902+LorenzoLiwanag@users.noreply.github.com>
2025-03-21 22:38:33 -0600

    some button functionalities

---
+++ Calculator.js
@@ -0,0 +1,25 @@
+const screen = document.getElementById("display-screen");
+const numberButtons = document.querySelectorAll(".numberBtn");
+const operationButtons = document.querySelectorAll(".operationBtn");
+const deleteBtn = document.getElementById("deleteBtn");
+const clearBtn = document.getElementById("clearBtn");
+
+numberButtons.forEach(button => {
+    button.addEventListener("click", ()=> {
+        screen.value += button.innerHTML;
+    });
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener("click", ()=> {
+        screen.value += button.innerHTML;
+    });
+})
+
+deleteBtn.addEventListener("click", () => {
+    screen.value = screen.value.slice(0, -1);
+})
+
+clearBtn.addEventListener("click", () => {
+    screen.value = null;
+})
